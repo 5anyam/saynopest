683e54e654e31f7a3021e170ef4bfd12355aebb2
Sanyam <you@example.comsanyamgulia@gmail.com>
2025-05-04 23:25:09 +0530

    Initial commit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,14 +5,11 @@ export default async function Page() {
       <h1 className="text-6xl font-bold text-amber-700">404</h1>
       <p className="mt-4 text-2xl text-gray-800">Oops! Page not found.</p>
       <p className="mt-2 text-gray-600">
-        The page you're looking for doesn't exist.
+        The page you&rsquo;re looking for doesn&rsquo;t exist.
       </p>
-      <a
-        href="/"
-        className="mt-6 px-4 py-2 bg-amber-700 text-white rounded hover:bg-amber-700 transition"
-      >
+      <h1 className="mt-6 px-4 py-2 bg-amber-700 text-white rounded hover:bg-amber-700 transition">
         Go back home
-      </a>
+      </h1>
     </div>
   );
 }
