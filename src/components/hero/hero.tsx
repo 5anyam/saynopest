b51fc549f896d3e3ef3c1fa8bf359e1e5b141695
Sanyam <you@example.comsanyamgulia@gmail.com>
2025-05-17 00:15:22 +0530

    Initial commit

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -14,7 +14,7 @@ export function Hero() {
                 Contact Us
             </a> 
         </div>
-        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+        <div className="hidden ml-20 lg:mt-0 h-96 lg:col-span-5 lg:flex">
             <img src="pest-service.jpg" alt="pest-control"/>
         </div>                
     </div>
@@ -65,7 +65,7 @@ export function Hero() {
 </div>
 
     <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-        <img className="w-full" src="home-img2.png" alt="dashboard image"/>
+        <img className="w-full" src="home-img2.jpg" alt="dashboard image"/>
         <div className="mt-4 md:mt-0">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">What we do at SayNoPest</h2>
             <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Our goal is simple – we are here to help you connect with good pest control service providers across the United States (both nationwide and locally). At SayNoPest, we compare rates, services, pest products, features, and customer reviews so that you can make the right decision. Our primary concern is to give you the best fit for your household and businesses. You can count on  SayNoPest- your go-to provider of the best pest control companies across the U.S.</p>
