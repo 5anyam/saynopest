3237a65fe6fac1785b60028035fc92476d52c75b
Sanyam <you@example.comsanyamgulia@gmail.com>
2025-07-08 16:10:02 +0530

    Initial commit

--- src/app/services/millipede/page.tsx
+++ src/app/services/millipede/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const millipedeTypes = [
   {
-    name: "American Jaint Millipede",
+    name: "American Gaint Millipede",
     image: "/types/millipede/american-jaint-millipede.jpg",
   },
   {
