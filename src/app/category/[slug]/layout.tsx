e311255dae24507a8bce39b40419ca989c5e5048
Sanyam <you@example.comsanyamgulia@gmail.com>
2025-07-21 13:40:12 +0530

    Initial commit

---
+++ src/app/category/[slug]/layout.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+interface BlogLayoutProps {
+  children: React.ReactNode;
+  params: Promise<{ slug: string }>;
+}
+
+export default async function BlogLayout({ children, params }: BlogLayoutProps) {
+  const resolvedParams = await params;
+  const canonicalUrl = `https://www.saynopest.com/category/${resolvedParams.slug}`;
+
+  return (
+    <html>
+      <head>
+        <link rel="canonical" href={canonicalUrl}/>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
