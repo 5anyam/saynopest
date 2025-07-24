b858c54fe132006f7690063b6779bf9290761955
Sanyam <you@example.comsanyamgulia@gmail.com>
2025-07-24 17:17:08 +0530

    Initial commit

--- src/components/clients.tsx
+++ src/components/clients.tsx
@@ -92,6 +92,8 @@ export function Clients(){
 
         <hr className="my-12 border-gray-200 dark:border-gray-700"/>
 
+        <h1 className="text-center text-green-500 text-2xl"></h1>
+
         <div className="grid grid-cols-2 gap-6 md:grid-cols-6 lg:grid-cols-6">
             <div className="flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1">
                 <img className="h-20 w-28" src="orkin.png" alt="orkin"/>
